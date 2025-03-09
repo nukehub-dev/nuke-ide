62d826492d4e782d1059d47667862665ff316c9a
Ahnaf Tahmid Chowdhury <tahmid@nukehub.org>
2025-03-09 17:44:43 +0600

    serve resources folder

--- applications/browser/webpack.config.js
+++ applications/browser/webpack.config.js
@@ -1,19 +1,19 @@
-/**
- * This file can be edited to customize webpack configuration.
- * To reset delete this file and rerun theia build again.
- */
 // @ts-check
 const configs = require('./gen-webpack.config.js');
 const nodeConfig = require('./gen-webpack.node.config.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
-/**
- * Expose bundled modules on window.theia.moduleName namespace, e.g.
- * window['theia']['@theia/core/lib/common/uri'].
- * Such syntax can be used by external code, for instance, for testing.
-configs[0].module.rules.push({
-    test: /\.js$/,
-    loader: require.resolve('@theia/application-manager/lib/expose-loader')
-}); */
+// Add CopyWebpackPlugin to copy resources
+configs.forEach(config => {
+    config.plugins.push(
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, '../../resources'), to: '.' }
+            ]
+        })
+    );
+});
 
 module.exports = [
     ...configs,
