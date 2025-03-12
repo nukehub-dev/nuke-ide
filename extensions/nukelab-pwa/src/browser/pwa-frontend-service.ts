838116c3501d56bd46775ccbed058342b47fd26a
Ahnaf Tahmid Chowdhury <tahmid@nukehub.org>
2025-03-13 03:24:43 +0600

    nukelab may provide the manifest

--- extensions/nukelab-pwa/src/browser/pwa-frontend-service.ts
+++ extensions/nukelab-pwa/src/browser/pwa-frontend-service.ts
@@ -4,8 +4,22 @@ import { FrontendApplication, FrontendApplicationContribution } from '@theia/cor
 @injectable()
 export class PWAFrontendService implements FrontendApplicationContribution {
     onStart(app: FrontendApplication): void {
+        this.addManifest();
+        this.registerServiceWorker();
+    }
+
+    private addManifest(): void {
+        if (!document.querySelector('link[rel="manifest"]')) {
+            const link = document.createElement('link');
+            link.rel = 'manifest';
+            link.href = '/hub/static/manifest.json';
+            document.head.appendChild(link);
+        }
+    }
+
+    private registerServiceWorker(): void {
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/service-worker.js')
+            navigator.serviceWorker.register('service-worker.js')
                 .then(reg => console.log('Service Worker registered:', reg))
                 .catch(err => console.error('Service Worker registration failed:', err));
         }
